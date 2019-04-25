7e7dbb805688f36231fce03130a16c2a3e95495f
Keith Burgie <krburgie@gmail.com>
2019-04-25 21:11:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,18 +1,15 @@
 numbers = [2, 4, 6]
 
-#def square_array(numbers)
-
-#end
-
-
 def square_array(numbers)
-  #new_numbers = []
-  #numbers.each do |element|
-  #new_numbers << element ** 2
-  #end
-  # new_numbers
   
-  numbers.collect { |number| number ** 2 }
+  new_numbers = []
+  numbers.each do |element|
+   new_numbers << element ** 2
+  end
+  new_numbers
+  
+  #numbers.collect { |number| number ** 2 }
+  
 end
 
 square_array(numbers)
